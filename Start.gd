44403a62133896fb46614273d0676ab4303b707f
Sam Huang <samestimable2016@gmail.com>
2021-06-29 08:50:16 -0700

    Added mob spawn bg in intro

--- Start.gd
+++ Start.gd
@@ -1,9 +1,29 @@
-extends CanvasLayer
+extends Node
+
+export (PackedScene) var Mob
 
 func activate_game():
 	get_tree().change_scene("res://InGame.tscn")
 
+func spawn_mob():
+	$MobSpawnPath/MobSpawnPoint.offset = randi()
+	
+	var mob = Mob.instance()
+	add_child(mob)	
+	
+	var direction = $MobSpawnPath/MobSpawnPoint.rotation + PI / 2
+	direction += rand_range(-PI / 4, PI / 4)
+	
+	mob.position = $MobSpawnPath/MobSpawnPoint.position
+	mob.rotation = direction
+	
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
 
 
 func _on_StartButton_pressed():
 	activate_game()
+
+
+func _on_MobSpawnTimer_timeout():
+	spawn_mob()
