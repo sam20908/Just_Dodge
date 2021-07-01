cf562633d71588005973054601772d93b044a467
Sam Huang <samestimable2016@gmail.com>
2021-07-01 13:45:48 -0700

    Implemented increase difficulty

--- GlobalState.gd
+++ GlobalState.gd
@@ -1,3 +1,19 @@
 extends Node
 
 var score = 0
+
+const DIFFICULTY_NAMES = {
+    1: "Easy",
+    2: "Medium",
+    3: "Hard",
+    4: "Insane",
+    5: "Impossible"
+}
+
+const DIFFICULTY_COLORS = {
+    1: Color(0.176471, 0.780392, 0.141176), # green
+    2: Color(0.780392, 0.694118, 0.141176), # yellow
+    3: Color(0.854902, 0.258824, 0.160784), # orange
+    4: Color(0.556863, 0.160784, 0.854902), # purple
+    5: Color(0.160784, 0.839216, 0.854902)  # cyan
+}
--- InGame.gd
+++ InGame.gd
@@ -4,18 +4,58 @@ export (PackedScene) var Mob
 export var Time_Increment_Interval = 0.005
 
 onready var global_state = get_node('/root/GlobalState')
+onready var difficulty_increment_interval = $DifficultyIncrementTimer.wait_time
 
 var time = 0
 var score = 0
+var difficulty = 1
+var difficulty_time_elapsed = 0
+
+
+func set_difficulty_color(difficulty_int):
+	var difficulty_clamped = clamp(difficulty_int, 1, 5)
+	$DifficultyLabel.add_color_override("font_color", global_state.DIFFICULTY_COLORS[difficulty_clamped])
+
+func set_difficulty_text_pre_impossible(difficulty_int):
+	var difficulty_name = global_state.DIFFICULTY_NAMES[difficulty_int]
+	$DifficultyLabel.text = "%s: %ss left" % [difficulty_name, difficulty_increment_interval - difficulty_time_elapsed]
+
+func set_difficulty_text_post_impossible():
+	$DifficultyLabel.text = "Impossible (MAX)!"
+
+func increase_difficulty():
+	difficulty = clamp(difficulty + 1, 1, 5)
+	set_difficulty_color(difficulty)
+
+	if difficulty < 5:
+		difficulty_time_elapsed = -1
+		set_difficulty_text_pre_impossible(difficulty)
+
+		$MobSpawnTimer.wait_time = $MobSpawnTimer.wait_time - 0.5
+	else:
+		set_difficulty_text_post_impossible()
+		$DifficultyIncrementTimer.disconnect("timeout", self, "_on_DifficultyIncrementTimer_timeout")
+		$DifficultyElapsedTimer.disconnect("timeout", self, "_on_DifficultyElapsedTimer_timeout")
+
+func increase_difficulty_time_elapsed():
+	difficulty_time_elapsed += 1
+	if difficulty < 5:
+		set_difficulty_text_pre_impossible(difficulty)
 
 func _ready():
 	$Player.start($Player.position)
 	# warning-ignore:return_value_discarded
 	$Player.connect("hit", self, "game_over")
 
+	set_difficulty_color(difficulty)
+	# set_difficulty_text_pre_impossible(difficulty)
+
 func begin_game():
 	$MobSpawnTimer.start()
 	$TimeTimer.start()
+	$DifficultyIncrementTimer.start()
+	$DifficultyElapsedTimer.start()
+	set_difficulty_text_pre_impossible(difficulty)
 
 func spawn_mob():
 	$MobSpawnPath/MobSpawnPoint.offset = randi()
@@ -26,7 +66,7 @@ func spawn_mob():
 	
 	var direction = $MobSpawnPath/MobSpawnPoint.rotation + PI / 2
 	direction += rand_range(-PI / 4, PI / 4)
-	
+
 	mob.position = $MobSpawnPath/MobSpawnPoint.position
 	mob.rotation = direction
 	
@@ -61,3 +101,11 @@ func handle_mob_gone_off_screen():
 
 func _on_StartDelayTimer_timeout():
 	begin_game()
+
+
+func _on_DifficultyIncrementTimer_timeout():
+	increase_difficulty()
+
+
+func _on_DifficultyElapsedTimer_timeout():
+	increase_difficulty_time_elapsed()
